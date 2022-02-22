b79bd4dba9c2a00ee364e2d6f2ceee70e0b8cdff
alexstew2 <alexstewart2012@GMAIL.COM>
2022-02-22 07:24:32 -0600

    tictactoe update

--- Programming Fundamentals 1/tictactoe.py
+++ Programming Fundamentals 1/tictactoe.py
@@ -1,12 +1,43 @@
-first_to_move = input('Who moves first? ')
-if first_to_move == 'X' or first_to_move == 'x':
+# Project: Tic-tac-toe
+
+# Write a command-line program where two people can play tic-tac-toe against one another.
+
+# 1) Should display the board
+# 2) Should take player input for what box they play in each turn
+# 3) Should handle games ending in wins/losses
+# 4) Should handle games ending in a tie
+
+first_player = input('Who moves first? ')
+
+if first_player.upper() == 'X':
     print('X moves first!')
-elif first_to_move == 'O' or first_to_move == 'o':
+    second_player = 'O'
+elif first_player.upper() == 'O':
     print('O moves first!')
+    second_player = 'X'
 else:
     print('Unknown player')
+    
 
-i=0
-while i<3:
-    print('|' '|' )
-    i+=1
+board = [[1,2,3],[1,2,3],[1,2,3]]
+
+# Prints the current board state
+def make_board():
+    i = 0
+    for row in board:
+        print(board[i])
+        i += 1
+
+
+make_board()
+
+move_row = int(input('Select row: ')) - 1
+move_column = int(input('Select column: ')) - 1
+
+if board[move_row][move_column] == 'X' or board[move_row][move_column] == 'O':
+    print('This space is occupied, select another')
+else:
+    board[move_row][move_column] = first_player.upper()
+
+make_board()
+
